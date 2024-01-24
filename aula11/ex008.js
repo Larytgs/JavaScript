c5b5236b07a2b269ab1311955626ed345de2dbf5
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-01-24 13:37:37 -0300

    j

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+console.log('CONDIÇÂO SIMPLES:');
+
+var velocidade = 80.5
+console.log (`A velocidade do seu carro é ${velocidade}`)
+if (velocidade > 70){
+    console.log (`Voce ultrapassou o limite de velocidade! Multado!`)
+
+} 
+console.log(`Use sempre o cinto de segurança!`)
