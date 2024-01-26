27c4188ddd0f212968fc21eba7a4b0197110fd99
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-01-26 16:26:39 -0300

    m

--- aula14ex1/script.js
+++ aula14ex1/script.js
@@ -1,23 +1,13 @@
 
-function carregar() { //para carregar, coloquei no html(body)
+function contagem() { //para contar, coloquei no html
 
     var msg = window.document.getElementById('msg')
-    var foto = document.getElementById('imagem')
-    var data = new Date()  //data atual
-    var hora = data.getHours() //hora atual
-    
+    var com = window.document.getElementById('começo')
+    var fim = document.getElementById('fim')
+    var pas = document.getElementById('passos')
 
-    if (hora >= 7 && hora < 12){
-        msg.innerText = `Bom dia! Agora sao ${hora} horas.`
-        img.src= ("fotos/manha.png")
-        document.body.style.background = '#FEDC5A'
-    } else if (hora >= 12 && hora <= 18){
-        msg.innerHTML = `Boa tarde! Agora sao ${hora} horas.`
-        foto.innerHTML= '<img src="fotos/tarde.png">'
-        document.body.style.background = '#f59629'
-    } else{
-        msg.innerHTML = `Boa noite! Agora sao ${hora} horas.`
-        foto.innerHTML= '<img src="fotos/noite.png">'
-        document.body.style.background = '#1e4c5c'
+    while (com <= fim){
+        console.log(`${com} ->`)
+        com + pas
     }
 }
