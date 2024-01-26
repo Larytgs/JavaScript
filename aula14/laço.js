072d969b910b489d2d82aa92e8574d33a4889786
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-01-26 15:38:35 -0300

    nvs

---
+++ aula14/laço.js
@@ -0,0 +1,22 @@
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+//laço mais simples, pois voce escolhe quantas vezes vai fazer
+
+//esse faz o teste antes e executa o comando
+var c = 1
+while (c <= 6) { //enquanto o contador der 6..
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}
+
+//esse executa o comando e depois faz o teste
+var c = 1
+do {
+    console.log(`Passo ${s}`)
+    c++
+} while (c <= 6)
