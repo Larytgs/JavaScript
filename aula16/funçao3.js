696fda53a106bbd3fb731f2ec394f8d1ab2a268d
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-03-21 19:24:39 -0300

    att

--- aula16/funçao3.js
+++ aula16/funçao3.js
@@ -1,5 +1,5 @@
 let v = function(x) { //uma variavel recebe uma funçao
     return x*2 //o dobro
 }
-console.log(v(5))
-console.log(v(7))
+console.log('O dobro de 5 é', v(5))
+console.log('O dobro de 7 é', v(7))
--- aula16/funçao4.js
+++ aula16/funçao4.js
@@ -6,6 +6,8 @@ function fatorial(n){
     return fat
 }
 console.log(fatorial(5))
+let msg = ('O fatorial de 5 é '+ fatorial(5))
+console.log(msg)
 
 //fatorial de um numero 
 // 5! = 5 x 4 x 3 x 2 x 1 = 120
