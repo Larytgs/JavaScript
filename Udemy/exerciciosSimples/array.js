3827beec50beb7ffbec786746959e541d46ec699
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-04-12 14:43:53 -0300

    Update array.js

--- Udemy/exerciciosSimples/array.js
+++ Udemy/exerciciosSimples/array.js
@@ -1,17 +1,30 @@
-let times = ['Flamengo', 'Coxa', 'Botafogo'];
+const times = ['Flamengo', 'Coxa', 'Botafogo', 'Atlético', 'Cruzeiro'];
 
-console.log(times) //mostrar em lista
+console.log('Acessando os times: ', times) //mostrar em lista
+console.log('O segundo time é: ', times [1]) //mostrar o segundo time
+console.log('Sao',times.length, 'arrays.')
 console.table(times) //mostrar em tabela
 
-console.log(times [1]) //mostrar o segundo time
-let msg = 'O segundo time é ' + (times[1])
-console.log(msg)
+//Operaçoes com o array
 
-times.push('Vasco') //para adicionar mais no final
+times.unshift('Corinthians') //add no começo do array
+times.push('USA') //add no final do array 
 
+console.log('\nAdicionando mais dois times no inicio e no final:')
+console.table(times)
+console.log('Agora o tamanho da array tem:' ,times.length , 'times.')
+
+times.pop() //remove o ultimo elemento
+times.shift() //remove o primeiro elemento
+console.log('\nRemovendo o primeiro e o ultimo:')
 console.table(times)
 
+console.log('\nAdicionando um novo time na posição 2:')
+times.splice(2, 1, 'Palmeiras') //add um elemento em uma posição específica, e removendo o antigo
+         //1º coloca o indice q quer remover, e quantos elementos quer remover(ate 2), e add ate 2 
+console.table(times)
 
-//adicionar um elemento no final do seu array, use o método push() .
-//no início do seu array, use o método unshift() .
-//Se você deseja adicionar um elemento em uma posição específica do seu array, use o método splice()
+console.log('\nOutro exemplo, adicionando 2 times na posição 1')
+times.splice(1, 1, 'USA', 'Botafogo') //outro exemplo, removendo 1, add 2 
+console.log(times)
+
