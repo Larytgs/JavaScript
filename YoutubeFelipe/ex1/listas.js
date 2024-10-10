b990d95c0e38a6dd6e9ca4a585ce494bf338d1bf
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-10-10 17:41:09 -0300

    att

---
+++ YoutubeFelipe/ex1/listas.js
@@ -0,0 +1,2 @@
+const names = ['Fernanda', 'Joaana', 'Valentina', 'Gustavo']
+console.log(names)
---
+++ YoutubeFelipe/ex1/strings.js
@@ -0,0 +1,10 @@
+const mensage = 'Ola mundoooo'
+console.log(mensage.length) //mostra qntdd de letras
+
+let firstName = 'Laryssa'
+let lastName = 'Silva'
+console.log('Meu nome é', firstName, lastName)
+console.log(`Meu nome é ${firstName} ${lastName} - usando cifrao e 2 chaves`)
+
+const names = 'Felipe, Julia, Matheus, Sara'
+console.log(names.split('.')) //colocando em lista
